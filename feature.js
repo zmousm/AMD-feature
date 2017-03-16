0773db991a26b4b1465582133f219e474cd66be4
Zenon Mousmoulas <zmousm@noc.grnet.gr>
2017-03-16 17:35:01 +0200

    Indentation

--- feature.js
+++ feature.js
@@ -32,79 +32,80 @@
 
       req([implementations], function(implementations) {
 
-      var i, impl, m, toLoad,
-          featureInfo = implementations[name],
-          hasMultipleImpls = Object.prototype.toString.call(featureInfo) === '[object Array]';
+	var i, impl, m, toLoad,
+            featureInfo = implementations[name],
+            hasMultipleImpls = Object.prototype.toString.call(featureInfo) === '[object Array]';
 
-      if (config.isBuild && hasMultipleImpls) {
-        // In build context, we want all possible
-        // implementations included.
-        for (i = 0, m = featureInfo.length; i < m; i++) {
-          impl = featureInfo[i].implementation;
-          if (typeof impl === 'function') {
-            impl = impl.call(featureInfo[i], name);
-          }
-          if (impl) {
+	if (config.isBuild && hasMultipleImpls) {
+          // In build context, we want all possible
+          // implementations included.
+          for (i = 0, m = featureInfo.length; i < m; i++) {
+            impl = featureInfo[i].implementation;
+            if (typeof impl === 'function') {
+              impl = impl.call(featureInfo[i], name);
+            }
+            if (impl) {
 	      if (preemptDefinition(config)) {
 		req([toLoad], function(alias) {
 		  define(name, [], alias);
 		  load(alias);
 		});
 	      } else {
-            req([impl], load);
-          }
-        }
+		req([impl], load);
+	      }
+            }
           }
 
-        // We're done here now.
-        return;
-      }
+          // We're done here now.
+          return;
+	}
 
-      if (hasMultipleImpls) {
-        // We have different implementations available,
-        // test for the one to use.
-        for (i = 0, m = featureInfo.length; i < m; i++) {
-          impl = featureInfo[i];
-          if (isAvailable(impl, name)) {
-            break;
+	if (hasMultipleImpls) {
+          // We have different implementations available,
+          // test for the one to use.
+          for (i = 0, m = featureInfo.length; i < m; i++) {
+            impl = featureInfo[i];
+            if (isAvailable(impl, name)) {
+              break;
+            }
+            impl = null;
           }
-          impl = null;
-        }
-      } else {
-        impl = featureInfo;
-        // The only implementation can have isAvailable check
-        if (impl && typeof impl === 'object' && ('isAvailable' in impl) && !isAvailable(impl, name)) {
-          impl = null;
-        }
-      }
+	} else {
+          impl = featureInfo;
+          // The only implementation can have isAvailable check
+          if (impl && typeof impl === 'object' && ('isAvailable' in impl) &&
+	      !isAvailable(impl, name)) {
+            impl = null;
+          }
+	}
 
-      if (impl) {
-        if (typeof impl === 'string') {
-          toLoad = impl;
-        } else if (typeof impl === 'object' && ('module' in impl)) {
+	if (impl) {
+          if (typeof impl === 'string') {
+            toLoad = impl;
+          } else if (typeof impl === 'object' && ('module' in impl)) {
             toLoad = typeof impl.module === 'function' ? impl.module(name) : impl.module;
 	    if (preemptDefinition(config)) {
 	      define(name, [], toLoad);
 	    }
 	    load(toLoad);
-          return;
-        } else {
-          toLoad = impl.implementation;
-          if (typeof toLoad === 'function') {
-            toLoad = impl.implementation(name);
+            return;
+          } else {
+            toLoad = impl.implementation;
+            if (typeof toLoad === 'function') {
+              toLoad = impl.implementation(name);
+            }
           }
-        }
 	  if (preemptDefinition(config)) {
 	    req([toLoad], function(alias) {
 	      define(name, [], alias);
 	      load(alias);
 	    });
 	  } else {
-        req([toLoad], load);
+            req([toLoad], load);
 	  }
-      } else {
-        req([], load);
-      }
+	} else {
+          req([], load);
+	}
 
       });
 
